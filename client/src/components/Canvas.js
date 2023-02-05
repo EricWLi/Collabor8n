3d5e327b7e44b68ae5e5ee75b0b9602a6e11e673
Eric W. Li <eric@ericwli.com>
2023-02-05 16:40:09 -0500

    Implement drawing on canvas

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -1,8 +1,71 @@
-function Canvas({width, height}) {
-  return (
-    <canvas>
+import { useState, useEffect, useRef } from 'react';
 
-    </canvas>
+function Canvas({ width, height }) {
+  const [mouseDown, setMouseDown] = useState(false);
+  const [strokes, setStrokes] = useState([]);
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = canvasRef.current.getContext('2d');
+
+    strokes.forEach(stroke => {
+      const first = stroke[0];
+      ctx.beginPath();
+      ctx.moveTo(first.x, first.y);
+
+      // Canvas does not allow single point lines, so use a rectangle instead
+      if (stroke.length < 2) {
+        ctx.fillRect(first.x, first.y, 1, 1);
+      }
+
+      for (let i = 1; i < stroke.length; i++) {
+        ctx.lineTo(stroke[i].x, stroke[i].y);
+      }
+
+      ctx.stroke();
+    });
+
+  });
+
+  function handleMouseDown(e) {
+    setStrokes([...strokes, [
+      {
+        x: e.clientX,
+        y: e.clientY
+      }
+    ]]);
+
+    setMouseDown(true);
+  }
+
+  function handleMouseUp(e) {
+    setMouseDown(false);
+  }
+
+  function handleMouseMove(e) {
+    if (!mouseDown || !strokes) {
+      return;
+    }
+
+    setStrokes(prevStrokes => {
+      const currStroke = prevStrokes[prevStrokes.length - 1];
+      return [...prevStrokes.slice(0, prevStrokes.length - 1), [...currStroke, { x: e.clientX, y: e.clientY }]];
+    })
+  }
+
+  return (
+    <>
+      <canvas 
+        className="canvas"
+        ref={canvasRef}
+        width={width} 
+        height={height}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+      >
+      </canvas>
+    </>
   );
 }
 
--- client/src/components/Whiteboard.js
+++ client/src/components/Whiteboard.js
@@ -1,11 +1,30 @@
+import { useState, useEffect } from "react";
 import Canvas from "./Canvas";
 import Toolbar from "./Toolbar";
 
 function Whiteboard() {
-  <>
-    <Canvas />
-    <Toolbar />
-  </>
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  })
+
+  return (
+    <>
+      <Canvas width={width} height={height} />
+      <Toolbar />
+    </>
+  );
 }
 
 export default Whiteboard;
