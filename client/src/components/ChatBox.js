0625a6acb47405ef10903038b5bdd644cee8e4bd
Eric W. Li <eric@ericwli.com>
2023-02-14 12:45:33 -0500

    Create chat box floating action button

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -1,11 +1,26 @@
 import { useState } from 'react';
 import { socket } from './Whiteboard';
+import { Fab } from '@mui/material';
+import { ChatRightText, ArrowDown } from 'react-bootstrap-icons';
 
 function ChatBox() {
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <>
-        </>
+        <div className='chat-container'>
+            <div className='chat-box'>
+                
+            </div>
+
+            <div className='chat-button'>
+                <Fab onClick={() => setIsOpen(!isOpen)}>
+                    { isOpen ? 
+                        <ArrowDown size={24} /> : 
+                        <ChatRightText size={24} />
+                    }
+                </Fab>
+            </div>
+        </div>
     );
 }
 
