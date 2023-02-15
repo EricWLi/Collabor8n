de4e7abd0c1800d3160dcfa2ab6ab43943527425
Eric W. Li <eric@ericwli.com>
2023-02-14 22:50:34 -0500

    Styling for chatbox & messages

--- client/src/components/ChatBox.js
+++ client/src/components/ChatBox.js
@@ -1,26 +1,37 @@
 import { useState } from 'react';
 import { socket } from './Whiteboard';
-import { Fab } from '@mui/material';
+import { Fab, Badge } from '@mui/material';
 import { ChatRightText, ArrowDown } from 'react-bootstrap-icons';
+import ChatMessage from './ChatMessage';
 
 function ChatBox() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
     return (
-        <div className='chat-container'>
-            <div className='chat-box'>
-                
-            </div>
+        <>
+            {open && 
+                <div className='chatbox'>
+                    <div className='chatbox-messages'>
+                        <ChatMessage name='Eric' message='Hello world!' />
+                    </div>
+
+                    <div className='chatbox-msgbox'>
+                        <textarea></textarea>
+                    </div>
+                </div>
+            }
 
             <div className='chat-button'>
-                <Fab onClick={() => setIsOpen(!isOpen)}>
-                    { isOpen ? 
+                <Fab onClick={() => setOpen(!open)}>
+                    {open ? 
                         <ArrowDown size={24} /> : 
-                        <ChatRightText size={24} />
+                        <Badge color='primary' badgeContent={0}>
+                            <ChatRightText size={24} />
+                        </Badge>
                     }
                 </Fab>
             </div>
-        </div>
+        </>
     );
 }
 
