9247007d33d4b6aeb727f2f5436aa6757a594fc9
Eric W. Li <eric@ericwli.com>
2023-03-08 18:47:11 -0500

    Added options to filter whiteboards

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Container, Grid } from '@mui/material';
+import { Box, Button, Container, FormControl, Grid, InputLabel, MenuItem, Select } from '@mui/material';
 import useRequireAuth from '../hooks/useRequireAuth';
 import useUserBoards from '../hooks/useUserBoards';
 import HeaderBar from '../components/HeaderBar';
@@ -11,6 +11,8 @@ function Dashboard() {
   const user = useRequireAuth();
   const { boards, createNewBoard, deleteBoard } = useUserBoards();
   const [idToDelete, setIdToDelete] = useState(null);
+  const [filter, setFilter] = useState('all');
+  const [orderBy, setOrderBy] = useState('lastUpdated');
   const navigate = useNavigate();
 
   if (!user || boards.error) {
@@ -34,15 +36,41 @@ function Dashboard() {
     setIdToDelete(null);
   }
 
-  const shouldOpenDeleteDialog = idToDelete !== null;
+  const filteredBoards = boards
+    .filter(board => {
+      switch (filter) {
+        case 'owner':
+          return board.owner._id === user.userId;
+        case 'shared':
+          return board.collaborators.includes(user.userId)
+        default:
+          return true;
+      }
+    })
+    .sort((a, b) => {
+      switch (orderBy) {
+        case 'lastCreated':
+          return a.createdAt > b.createdAt ? -1 : 1;
+        case 'lastUpdated':
+          return a.updatedAt > b.updatedAt ? -1 : 1;
+        case 'firstCreated':
+          return a.createdAt < b.createdAt ? -1 : 1;
+        case 'firstUpdated':
+          return a.updatedAt < b.updatedAt ? -1 : 1;
+        default:
+          return 0;
+      }
+    });
 
-  const boardCards = boards.map(board => {
+  const boardCards = filteredBoards.map(board => {
     return (
       <Grid item xs={12} sm={6} lg={4} xl={3} key={board._id}>
         <ThumbnailCard board={board} onDelete={id => setIdToDelete(id)} />
       </Grid>
     )
   });
+
+  const shouldOpenDeleteDialog = idToDelete !== null;
 
   return (
     <Container>
@@ -68,7 +96,38 @@ function Dashboard() {
           </Button>
         </Grid>
       </Grid>
-      
+
+      <Box className="filter">
+        <FormControl sx={{ m: 1, minWidth: 180 }}>
+          <InputLabel id="filter-label">Filter</InputLabel>
+          <Select
+            labelId="filter-label"
+            id="filter-select"
+            defaultValue="all"
+            onChange={e => setFilter(e.target.value)}
+          >
+            <MenuItem value="all">All</MenuItem>
+            <MenuItem value="owner">My Boards</MenuItem>
+            <MenuItem value="shared">Shared with me</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, minWidth: 180 }}>
+          <InputLabel id="orderby-label">Order By</InputLabel>
+          <Select
+            labelId="orderby-label"
+            id="orderby-select"
+            defaultValue="lastUpdated"
+            onChange={e => setOrderBy(e.target.value)}
+          >
+            <MenuItem value="lastCreated">Last Created</MenuItem>
+            <MenuItem value="lastUpdated">Last Updated</MenuItem>
+            <MenuItem value="firstCreated">First Created</MenuItem>
+            <MenuItem value="firstUpdated">First Updated</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+
       <Grid
         container
         spacing={3}
@@ -77,9 +136,9 @@ function Dashboard() {
       </Grid>
 
       <DeleteConfirmation
-        open={shouldOpenDeleteDialog} 
+        open={shouldOpenDeleteDialog}
         handleConfirm={handleDeleteConfirm}
-        handleCancel={handleDeleteCancel} 
+        handleCancel={handleDeleteCancel}
       />
     </Container>
   );
